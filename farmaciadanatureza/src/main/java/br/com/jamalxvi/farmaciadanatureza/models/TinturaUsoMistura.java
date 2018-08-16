549feed73b5f99b331cd88f18ffdbe9aff4e0c32
jamalxvi <henriquearantest@gmail.com>
2018-08-15 23:42:45 -0300

    Creating new modeling for Tintura object. Adding the possibility of mix Tintura

---
+++ farmaciadanatureza/src/main/java/br/com/jamalxvi/farmaciadanatureza/models/TinturaUsoMistura.java
@@ -0,0 +1,33 @@
+package br.com.jamalxvi.farmaciadanatureza.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+/**
+ * Esta classe representa quando há mistura de tintura, a classe que faz a ligação do
+ * uso da tintura com sua proporção. Seus atributos são:<br/>
+ * -Entidade Base;
+ * -Qual é a Dosagem de Tintura Escolhida
+ * -Qual é a proporção a se usar
+ * @author Jamal XVI <henriquearantest@gmail.com>
+ * @version 0.1
+ * @since 0.1
+ */
+@Entity
+@Table(name = "TINTURA_USO_MISTURA")
+@AttributeOverrides(value = {
+        @AttributeOverride(name = "id", column = @Column(name = "ID_TIN_USO_MIS")),
+        @AttributeOverride(name = "versao", column = @Column(name = "VER_TIN_USO_MIS")),
+        @AttributeOverride(name = "dataCriacao", column = @Column(name = "DAT_CRI_TIN_USO_MIS"))
+})
+@Builder
+@Data
+public class TinturaUsoMistura extends EntidadeBase{
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = TinturaDosagensEstoque.class)
+    @JoinColumn(name = "ID_TIN_DOS_USO_EST")
+    private TinturaDosagensUsoEstoque dosagem;
+    @Column(name = "PROP_TIN_USO_MIS")
+    private BigDecimal proporcao;
+}
