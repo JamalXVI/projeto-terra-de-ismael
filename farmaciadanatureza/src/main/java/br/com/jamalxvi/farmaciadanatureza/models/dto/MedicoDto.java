38098282a9d604ebb2dd3c06c019604646163af3
Henrique Arantes Tiraboschi <henriquearantest@gmail.com>
2019-01-04 14:31:12 -0200

    Update MedicoDto
    Update the Documatation on MedicoDto

--- farmaciadanatureza/src/main/java/br/com/jamalxvi/farmaciadanatureza/models/dto/MedicoDto.java
+++ farmaciadanatureza/src/main/java/br/com/jamalxvi/farmaciadanatureza/models/dto/MedicoDto.java
@@ -3,6 +3,14 @@ package br.com.jamalxvi.farmaciadanatureza.models.dto;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * Define o Dto de informações do Médico (dados não sensíveis) para ser exibida para o
+ * usuário do sistema
+ *
+ * @author Jamal XVI <henriquearantest@gmail.com>
+ * @version 0.1
+ * @since 0.1
+ */
 @Data
 @Builder
 public class MedicoDto {
