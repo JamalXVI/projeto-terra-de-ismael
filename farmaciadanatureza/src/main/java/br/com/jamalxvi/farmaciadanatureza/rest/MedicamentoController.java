b4032fdafae5308ae4272c04ffaaf8225ce57bc8
Henrique Arantes Tiraboschi <henrique.tiraboschi@evoluservices.com>
2019-01-08 16:04:45 -0200

    Created MedicamentoController
    Created the MedicamentoController, which is responsible to handle the generic use of medicines

---
+++ farmaciadanatureza/src/main/java/br/com/jamalxvi/farmaciadanatureza/rest/MedicamentoController.java
@@ -0,0 +1,27 @@
+package br.com.jamalxvi.farmaciadanatureza.rest;
+
+import br.com.jamalxvi.farmaciadanatureza.models.dto.ElementoDeListaDto;
+import br.com.jamalxvi.farmaciadanatureza.service.MedicamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping(value = "/api/medicines", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MedicamentoController {
+
+    @Autowired
+    private MedicamentoService medicamentoService;
+
+    @RequestMapping(method = GET, value = "/")
+    @PreAuthorize("hasRole('USUARIO')")
+    public List<ElementoDeListaDto> list() {
+        return medicamentoService.retornaListaDePossiveisMedicamentos();
+    }
+}
